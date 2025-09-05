620b7af10bdccc98be8faa5d2c32d916ed299995
Maximiliano Agustin Fernandez <machucho.fernandez@gmail.com>
2025-09-05 10:55:58 -0300

    cambio if por case

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #def
-def listas(sku,existencias):
-    lista1=[] #lista de sku
-    lista2=[] #lista de existencias
+def ingresar_sku(a, b):
+    sku=int(input("Ingrese el SKU: "))
+    lista1.append(sku)
+    existencias = int(input("Ingrese las existencias: "))
+    lista2.append(existencias)
+
+lista1=[] #lista de sku
+lista2=[] #lista de existencias
+
 
 print("""Bienvenido, seleccione una opción:
       1. Ingresar SKU
@@ -9,6 +15,22 @@ print("""Bienvenido, seleccione una opción:
       3. Eliminar SKU
       4. Salir""")
 seleccion=(int(input("Opción seleccionada: ")))
+
+match seleccion:
+    case 1:
+    case 2:
+        sku=int(input("Ingrese el SKU a buscar: "))
+        if sku in lista1:
+            print("El SKU",sku,"tiene",existencias,"existencias")
+        else:
+        
+            
+    case 3:
+        print("bbbbb")
+    case 4:
+        print("cccccc")
+    
+'''
 if seleccion==1:
     sku=int(input("Ingrese el SKU: "))
     lista1.append(sku)
@@ -26,3 +48,4 @@ elif seleccion==3:
         print("SKU eliminado correctamente")
     else:
         print("SKU no encontrado")
+'''
