82ce5b84fd61075ad66d66ecce629e6ea531c5bc
Maximiliano Agustin Fernandez <machucho.fernandez@gmail.com>
2025-10-31 09:57:10 -0300

    sintaxis

--- main.py
+++ main.py
@@ -4,63 +4,57 @@ from funcion import normalizar, ingresar, buscar, eliminar, modificar
 # programa principal
 try:
     matriz = []
-    print("""Bienvenido, seleccione una opción:
-        1. Ingresar 
-        2. Buscar 
-        3. Eliminar 
-        4. Modificar
-        5. Salir""")
 
     while True:
-        seleccion = input("Opción seleccionada: ")
+        seleccion = input("Ingrese que quiere hacer (0 para opciones): ")
         match normalizar(seleccion):
             case "0" | "cero" | "opciones":
                 print("""Opciones disponibles:
-            1. Ingresar 
-            2. Buscar 
-            3. Eliminar 
-            4. Modificar
+            1. Ingresar sku
+            2. Buscar
+            3. Eliminar sku
+            4. Modificar categorias
             5. Salir""")
 
             case "1" | "uno" | "ingresar" | "ingresar sku":
                 ingresar(matriz)
-                otro = normalizar(input("¿Desea ingresar otro SKU? (0=NO, 1=SÍ): "))
+                otro = normalizar(input("¿Desea ingresar otro SKU? (0=no, 1=sí): "))
                 while otro in ("1", "si", "yes"):
                     ingresar(matriz)
-                    otro = normalizar(input("¿Desea ingresar otro SKU? (0=NO, 1=SÍ): "))
-                while otro not in ("0", "1", "si", "yes"):
+                    otro = normalizar(input("¿Desea ingresar otro SKU? (0=no, 1=sí): "))
+                while otro not in ("0", "no", "1", "si", "yes"):
                     print("¡Error! Opción no válida…")
-                    otro = normalizar(input("¿Desea ingresar otro SKU? (0=NO, 1=SÍ): "))
+                    otro = normalizar(input("¿Desea ingresar otro SKU? (0=no, 1=sí): "))
 
             case "2" | "dos" | "buscar" | "buscar sku":
                 buscar(matriz)
-                otro = normalizar(input("¿Desea buscar otro SKU? (0=NO, 1=SÍ): "))
+                otro = normalizar(input("¿Desea buscar otro SKU? (0=no, 1=sí): "))
                 while otro in ("1", "si", "yes"):
                     buscar(matriz)
-                    otro = normalizar(input("¿Desea buscar otro SKU? (0=NO, 1=SÍ): "))
+                    otro = normalizar(input("¿Desea buscar otro SKU? (0=no, 1=sí): "))
                 while otro not in ("0", "1", "si", "yes"):
                     print("¡Error! Opción no válida…")
-                    otro = normalizar(input("¿Desea buscar otro SKU? (0=NO, 1=SÍ): "))
+                    otro = normalizar(input("¿Desea buscar otro SKU? (0=no, 1=sí): "))
 
             case "3" | "tres" | "eliminar" | "eliminar sku":
                 eliminar(matriz)
-                otro = normalizar(input("¿Desea eliminar otro SKU? (0=NO, 1=SÍ): "))
+                otro = normalizar(input("¿Desea eliminar otro SKU? (0=no, 1=sí): "))
                 while otro in ("1", "si", "yes"):
                     eliminar(matriz)
-                    otro = normalizar(input("¿Desea eliminar otro SKU? (0=NO, 1=SÍ): "))
+                    otro = normalizar(input("¿Desea eliminar otro SKU? (0=no, 1=sí): "))
                 while otro not in ("0", "1", "si", "yes"):
                     print("¡Error! Opción no válida…")
-                    otro = normalizar(input("¿Desea eliminar otro SKU? (0=NO, 1=SÍ): "))
+                    otro = normalizar(input("¿Desea eliminar otro SKU? (0=no, 1=sí): "))
 
             case "4" | "cuatro" | "modificar":
                 modificar(matriz)
-                otro = normalizar(input("¿Desea modificar otro SKU? (0=NO, 1=SÍ): "))
+                otro = normalizar(input("¿Desea modificar otro SKU? (0=no, 1=sí): "))
                 while otro in ("1", "si", "yes"):
                     modificar(matriz)
-                    otro = normalizar(input("¿Desea modificar otro SKU? (0=NO, 1=SÍ): "))
+                    otro = normalizar(input("¿Desea modificar otro SKU? (0=no, 1=sí): "))
                 while otro not in ("0", "1", "si", "yes"):
                     print("¡Error! Opción no válida…")
-                    otro = normalizar(input("¿Desea modificar otro SKU? (0=NO, 1=SÍ): "))
+                    otro = normalizar(input("¿Desea modificar otro SKU? (0=no, 1=sí): "))
 
             case "5" | "cinco" | "salir":
                 break
